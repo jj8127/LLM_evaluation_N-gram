859c80c162a5208aca2aa1ccc95a995b99693f02
jj8127 <jj81271000@gmail.com>
2025-01-14 09:37:31 +0900

    eval2

--- ollama_model_load.py
+++ ollama_model_load.py
@@ -3,4 +3,7 @@ from langchain_core.output_parsers import StrOutputParser
 from langchain_core.prompts import ChatPromptTemplate
 
 # LangChain이 지원하는 다른 채팅 모델을 사용합니다. 여기서는 Ollama를 사용합니다.
-llm = ChatOllama(model="DUCChatbot5ep:latest", stop=["</s>"])
+llama = ChatOllama(model="llama3.2:latest", stop=["</s>"])
+DUChatbot15ep = ChatOllama(model="DUchatbot:latest", stop=["</s>"])
+DUChatbot10ep = ChatOllama(model="DUChatbot10ep:latest", stop=["</s>"])
+DUChatbot5ep = ChatOllama(model="DUCChatbot5ep:latest", stop=["</s>"])
